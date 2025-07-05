d1960208f87e82dcec798fcfd6259497c7691ffa
Aaron Sadler <aaron@umbhost.net>
2025-07-05 20:22:22 +0100

    Changed to use item name

--- Umbraco.Community.UmbNav/src/components/umbnav-group/umbnav-group.data.ts
+++ Umbraco.Community.UmbNav/src/components/umbnav-group/umbnav-group.data.ts
@@ -78,7 +78,7 @@ export async function generateUmbNavLink(context: UmbControllerHost, item: Model
                     return {
                         ...item,
                         icon: await itemDataResolver.getIcon(),
-                        name: await itemDataResolver.getName(),
+                        name: item.name,
                         description: await getDocumentUrl(context, item.contentKey as Guid),
                     };
                 }
